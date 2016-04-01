e7f6d27a6f28727e662e0cd31868bfb5a262ddda
Kuzeko <web@kuzeko.com>
2016-04-01 14:52:59 +0200

    new class for Weighted edges

---
+++ src/main/java/eu/unitn/disi/db/grava/graphs/WeightedEdge.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2016 Kuzeko
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package eu.unitn.disi.db.grava.graphs;
+
+/**
+ *
+ * @author Kuzeko
+ */
+public class WeightedEdge extends Edge {
+    
+    private double weight = 0.0;
+    
+    public WeightedEdge(long src, long dest, long rel) {
+        this(src, dest, rel, 0.0);
+        
+    }
+    
+    public WeightedEdge(long src, long dest, long rel, double weight) {
+        super(src, dest, rel);
+        this.weight = weight;
+    }
+
+    
+    /**
+     * 
+     * @return  the weight of the edge
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    
+    /**
+     * 
+     * @param weight  change the weight of the edge
+     */
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return this.getSource() + "-[" + this.getLabel() + " : " + this.weight+"]->" + this.getDestination();
+    }
+    
+    @Override
+    public WeightedEdge reversedEdge() {
+        return new WeightedEdge(this.getDestination(), this.getSource(), this.getLabel(), weight);
+    }
+}
+    
+
